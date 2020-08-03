7ca123a154e84e24fa3c3618aa1f27b39066795a
zeke2856 <46726503+zeke2856@users.noreply.github.com>
2020-08-03 19:02:28 -0400

    refinement

--- BBwall/Sorting Code/PlatformIOtest-6-12-20/src/main.cpp
+++ BBwall/Sorting Code/PlatformIOtest-6-12-20/src/main.cpp
@@ -1,42 +1,116 @@
 #include <Arduino.h>
-
+#include <SPI.h>
 #include <Wire.h>
- 
+#include "Adafruit_TCS34725.h"
+
+// Pick analog outputs, for the UNO these three work well
+// use ~560  ohm resistor between Red & Blue, ~1K for green (its brighter)
+#define redpin 3
+#define greenpin 5
+#define bluepin 6
+// for a common anode LED, connect the common pin to +5V
+// for common cathode, connect the common to ground
+
+// set to false if using a common cathode LED
+#define commonAnode true
+
+// our RGB -> eye-recognized gamma color
+byte gammatable[256];
+
+
+Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_154MS, TCS34725_GAIN_4X);
+
 void setup() {
-  Wire.begin();
-  Serial.begin(115200);
-  Serial.println("\nI2C Scanner");
-}
- 
-void loop() {
-  byte error, address;
-  int nDevices;
-  Serial.println("Scanning...");
-  nDevices = 0;
-  for(address = 1; address < 127; address++ ) {
-    Wire.beginTransmission(address);
-    error = Wire.endTransmission();
-    if (error == 0) {
-      Serial.print("I2C device found at address 0x");
-      if (address<16) {
-        Serial.print("0");
-      }
-      Serial.println(address,DEC);
-      nDevices++;
+  Serial.begin(9600);
+  Serial.println("Color View Test!");
+
+  if (tcs.begin()) {
+    Serial.println("Found sensor");
+  } else {
+    Serial.println("No TCS34725 found ... check your connections");
+    while (1); // halt!
+  }
+
+  // use these three pins to drive an LED
+#if defined(ARDUINO_ARCH_ESP32)
+  Serial.println("pin setup");
+  // ledcAttachPin(redpin, 1);
+  // ledcSetup(1, 12000, 8);
+  // ledcAttachPin(greenpin, 2);
+  // ledcSetup(2, 12000, 8);
+  // ledcAttachPin(bluepin, 3);
+  // ledcSetup(3, 12000, 8);
+  Serial.println("pin setdown");
+
+#else
+  pinMode(redpin, OUTPUT);
+  pinMode(greenpin, OUTPUT);
+  pinMode(bluepin, OUTPUT);
+#endif
+
+  // thanks PhilB for this gamma table!
+  // it helps convert RGB colors to what humans see
+  for (int i=0; i<256; i++) {
+    float x = i;
+    x /= 255;
+    x = pow(x, 2.5);
+    x *= 255;
+
+    if (commonAnode) {
+      gammatable[i] = 255 - x;
+    } else {
+      gammatable[i] = x;
     }
-    else if (error==4) {
-      Serial.print("Unknow error at address 0x");
-      if (address<16) {
-        Serial.print("0");
-      }
-      Serial.println(address,DEC);
-    }    
+    //Serial.println(gammatable[i]);
   }
-  if (nDevices == 0) {
-    Serial.println("No I2C devices found\n");
-  }
-  else {
-    Serial.println("done\n");
-  }
-  delay(5000);          
+}
+
+// The commented out code in loop is example of getRawData with clear value.
+// Processing example colorview.pde can work with this kind of data too, but It requires manual conversion to 
+// [0-255] RGB value. You can still uncomments parts of colorview.pde and play with clear value.
+void loop() {
+  float red, green, blue;
+  
+  tcs.setInterrupt(false);  // turn on LED
+
+   // takes 50ms to read
+while(true){
+ 
+ 
+  delay(3000); 
+  tcs.getRGB(&red, &green, &blue);
+  delay(100);
+
+  Serial.print("R:\t"); Serial.println(int(red)); 
+  Serial.print("G:\t"); Serial.println(int(green)); 
+  Serial.print("B:\t"); Serial.println(int(blue));
+
+  Serial.print("\n");
+}
+//  uint16_t red, green, blue, clear;
+//  
+//  tcs.setInterrupt(false);  // turn on LED
+//
+//  delay(60);  // takes 50ms to read
+//
+//  tcs.getRawData(&red, &green, &blue, &clear);
+//  
+//  tcs.setInterrupt(true);  // turn off LED
+//
+//  Serial.print("C:\t"); Serial.print(int(clear)); 
+//  Serial.print("R:\t"); Serial.print(int(red)); 
+//  Serial.print("\tG:\t"); Serial.print(int(green)); 
+//  Serial.print("\tB:\t"); Serial.print(int(blue));
+//  Serial.println();
+
+
+#if defined(ARDUINO_ARCH_ESP32)
+  ledcWrite(1, gammatable[(int)red]);
+  ledcWrite(2, gammatable[(int)green]);
+  ledcWrite(3, gammatable[(int)blue]);
+#else
+  analogWrite(redpin, gammatable[(int)red]);
+  analogWrite(greenpin, gammatable[(int)green]);
+  analogWrite(bluepin, gammatable[(int)blue]);
+#endif
 }
