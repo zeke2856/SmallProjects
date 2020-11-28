126bd16d5b1d0804525180166545f1980bc97a25
zeke2856 <46726503+zeke2856@users.noreply.github.com>
2020-11-28 08:58:11 -0500

    working sorter and final carousel path design
    yeehaw

---
+++ BBwall/Sorting Code/PlatformIOtest-6-12-20/src/ballIndex.h
@@ -0,0 +1,9 @@
+enum COLOR {Blank_ball, Black_ball, White_ball};
+int num_color_states = 3;
+const int BLANK[] = {58, 96, 94};
+const int BLACK[] = {52, 98, 92};
+const int WHITE[] = {57, 99, 83};
+
+class ballIndex {
+    public: 
+};
